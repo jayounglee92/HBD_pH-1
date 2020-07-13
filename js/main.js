5e8cdfe7c1d3710ca83086dfdd4236d14a4b5a9a
Jayoung Lee <39401563+jayounglee92@users.noreply.github.com>
2020-07-13 18:41:30 +0900

    블렌딩 캔버스 계산 완료, 오렌지 애니메이션추가

--- js/main.js
+++ js/main.js
@@ -95,10 +95,17 @@
             scrollHeight: 0,
             objs: {
                 container: document.querySelector('#scroll-section-3'),
-                canvasCaption: document.querySelector('.canvas-caption')
+                canvasCaption: document.querySelector('.canvas-caption'),
+                canvas: document.querySelector('.image-blend-canvas'),
+                context: document.querySelector('.image-blend-canvas').getContext('2d'),
+                imagesPath : [
+                    './images/blend-image-1.jpg',
+                    './images/blend-image-2.jpg'
+                ],
+                images: []
             },
             values: {
-    
+                
             }
         }
     ];
@@ -117,6 +124,14 @@
 			imgElem2.src = `../video/002/IMG_${7027 + i}.JPG`;
             sceneInfo[2].objs.videoImages.push(imgElem2);
         }
+
+        let imgElem3;
+        for(let i = 0; i < sceneInfo[3].objs.imagesPath.length; i++) {
+            imgElem3 = new Image();
+            imgElem3.src = sceneInfo[3].objs.imagesPath[i];
+            sceneInfo[3].objs.images.push(imgElem3);
+        }
+        console.log(sceneInfo[3].objs.images)
 
     }
     setCanvasImages();
@@ -284,6 +299,23 @@ function playAnimation() {
 
         case 3:
             // console.log('3 play');
+            // 가로세로 모두 꽉차게 하기위해 계산 필요
+            const widthRatio = window.innerWidth /  objs.canvas.width;
+            const heightRatio = window.innerHeight / objs.canvas.height;
+            let canvasScaleRatio;
+
+            if(widthRatio <= heightRatio) {
+                // 캔버스보다 브라우저가 홀쭉한 경우
+                canvasScaleRatio = heightRatio;
+            }else{
+                // 캔버스보다 브라우저 창이 납작한 경우
+                canvasScaleRatio = widthRatio;
+            }
+
+            objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
+            objs.context.fillStyle = 'white';
+            objs.context.drawImage(objs.images[0], 0, 0);
+
             break;
     }
 }
