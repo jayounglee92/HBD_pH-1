9aa68d3feefc9b913ed057f989858addbdb37782
Jayoung Lee <39401563+jayounglee92@users.noreply.github.com>
2020-07-15 18:19:08 +0900

    캔버스 드로우 애니메이션2

--- js/main.js
+++ js/main.js
@@ -105,7 +105,9 @@
                 images: []
             },
             values: {
-                
+                rect1X: [ 0, 0, { start: 0, end: 0 } ],
+                rect2X: [ 0, 0, { start: 0, end: 0 } ],
+                rectStartY: 0,
             }
         }
     ];
@@ -131,7 +133,7 @@
             imgElem3.src = sceneInfo[3].objs.imagesPath[i];
             sceneInfo[3].objs.images.push(imgElem3);
         }
-        console.log(sceneInfo[3].objs.images)
+        //console.log(sceneInfo[3].objs.images)
 
     }
     setCanvasImages();
@@ -312,10 +314,43 @@ function playAnimation() {
                 canvasScaleRatio = widthRatio;
             }
 
+            
             objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
             objs.context.fillStyle = 'white';
             objs.context.drawImage(objs.images[0], 0, 0);
 
+            // 캔버스 사이즈에 맞춰 가정한 innerWidth와 innerHeight
+            const recalculatedInnerWidth = document.body.offsetWidth / canvasScaleRatio;
+            const recalculatedInnerHeight = window.innerHeight / canvasScaleRatio;
+
+            if (!values.rectStartY) {
+                // values.rectStartY = objs.canvas.getBoundingClientRect().top;
+                values.rectStartY = objs.canvas.offsetTop + (objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2;
+                values.rect1X[2].start = (window.innerHeight / 2) / scrollHeight;
+                values.rect2X[2].start = (window.innerHeight / 2) / scrollHeight;
+                values.rect1X[2].end = values.rectStartY / scrollHeight;
+                values.rect2X[2].end = values.rectStartY / scrollHeight;
+            }
+
+            const whiteRectWidth = recalculatedInnerWidth * 0.15;
+            values.rect1X[0] = (objs.canvas.width - recalculatedInnerWidth) / 2;
+            values.rect1X[1] = values.rect1X[0] - whiteRectWidth;
+            values.rect2X[0] = values.rect1X[0] + recalculatedInnerWidth - whiteRectWidth;
+            values.rect2X[1] = values.rect2X[0] + whiteRectWidth;
+
+            // 좌우 흰색 박스 그리기
+            objs.context.fillRect(
+                parseInt(calcValues(values.rect1X, currentYOffset)),
+                0,
+                parseInt(whiteRectWidth),
+                objs.canvas.height
+            );
+            objs.context.fillRect(
+                parseInt(calcValues(values.rect2X, currentYOffset)),
+                0,
+                parseInt(whiteRectWidth),
+                objs.canvas.height
+            );
             break;
     }
 }
